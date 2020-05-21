1d51fadf4867c18a4694c952692974eebee089f3
reireynoso <reireynoso@gmail.com>
2020-05-20 22:14:01 -0400

    styled segment messages

--- frontend/src/components/SegmentConversation.js
+++ frontend/src/components/SegmentConversation.js
@@ -40,6 +40,15 @@ export default ({conversations,socket, conversation, conversation: {messages, us
             dispatch(selectConversationAction(conversation))
         }
     }
+
+    const truncate = () => {
+        const message = messages[messages.length-1].content
+        let shortenedMsg = message.slice(0,18) 
+        if(message.length > 18){
+            return shortenedMsg + '...'
+        }
+        return shortenedMsg
+    }
     //does not account if the conversation includes only the logged in user
     // const usersRefactored = () => {
     //     return users.filter(user => user.email !== loggedInUser.email)
@@ -49,12 +58,20 @@ export default ({conversations,socket, conversation, conversation: {messages, us
     // }
     const dispatch = useDispatch()
     return (
-        <div onClick={handleConversationSelect}>
-            <h3>{users[0].name}</h3>
-            <p>{messages[messages.length-1].content}</p>
-            {
-                typing && <p>Typing...</p>
-            }
+        <div className="segment" onClick={handleConversationSelect}>
+            <div className="segment__notification">
+                D
+            </div>
+            <div className="segment__info">
+                <img className="segment__image" src="https://cdn2.vectorstock.com/i/1000x1000/01/66/businesswoman-character-avatar-icon-vector-12800166.jpg"/>
+                <div className="segment__details">
+                    <h4>{users[0].name}</h4>
+                    <p>{truncate()}</p>
+                    {
+                        typing && <p>Typing...</p>
+                    }
+                </div>
+            </div>
         </div>
     )
 }
