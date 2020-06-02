424360b82c23da22c1ac2cb909b993b400dd1e2e
reireynoso <reireynoso@gmail.com>
2020-06-01 21:48:50 -0400

    styled message component

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 
-export default ({message, message: {content}}) => {
-    console.log(message)
+export default ({message: {content, user}}) => {
+    const loggedUser = useSelector(state => state.user)
+    // console.log(message)
     return (
-        <div>
-           {content}
+        <div className="message-container">
+            <div className={`message ${loggedUser.email === user.email ? "mine" : "other"}`}>
+                {content}
+            </div>
         </div>
     )
 }
